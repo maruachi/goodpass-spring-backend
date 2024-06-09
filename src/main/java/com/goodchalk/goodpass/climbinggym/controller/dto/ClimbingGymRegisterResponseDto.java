fd9a6cbca333f09544b27e6af48e3a712c9864aa
dgyim <yimdgkr@gmail.com>
2024-06-09 19:38:24 +0900

    climbinggym 등록 서비스 구현

---
+++ src/main/java/com/goodchalk/goodpass/climbinggym/controller/dto/ClimbingGymRegisterResponseDto.java
@@ -0,0 +1,27 @@
+package com.goodchalk.goodpass.climbinggym.controller.dto;
+
+import com.goodchalk.goodpass.climbinggym.domain.ClimbingGym;
+import jakarta.persistence.Column;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Setter
+public class ClimbingGymRegisterResponseDto {
+    private String climbingGymName;
+    private String address;
+    private String instagramAccount;
+    private LocalDateTime requestDateTime;
+    public static ClimbingGymRegisterResponseDto from(ClimbingGym climbingGym) {
+        return ClimbingGymRegisterResponseDto.builder()
+                .climbingGymName(climbingGym.getClimbingGymName())
+                .address(climbingGym.getAddress())
+                .instagramAccount(climbingGym.getInstagramAccount())
+                .requestDateTime(climbingGym.getRequestDateTime())
+                .build();
+    }
+}
